ab25edadc00122d5f40e55f31eae09659990b18e
sandeepvempati <vempatisandeep189@gmail.com>
2017-07-13 13:36:35 -0500

    insertlist

--- insertlist8.py
+++ insertlist8.py
@@ -2,3 +2,9 @@ li = []
 for i in range(int(raw_input("Enter number"))):
     li.append(int(raw_input()))
 print li
+
+# li = []
+# for i in range(int(raw_input("Enter number"))):
+#     a,b = raw_input().split()
+#     li.insert(int(a),b)
+# print li

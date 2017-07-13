feb2f0c4e41b0cc37089471799620c91561eddac
sandeepvempati <vempatisandeep189@gmail.com>
2017-07-13 12:24:11 -0500

    posnegprog

---
+++ posnegzero6.py
@@ -0,0 +1,7 @@
+n = int(raw_input("Enter a number"))
+if n>0:
+    print "Given number %d is positive" %n
+elif n<0:
+    print "Given number %d is negative" %n
+else:
+    print "Given number is zero"
